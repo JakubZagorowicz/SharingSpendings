d6a0902105876314719a2939dccdad5507d89087
User <user@macbook-pro-user.local>
2019-01-08 16:26:18 +0100

    file rename

---
+++ Modules/MeetingManagementModule/DebtCell.swift
@@ -0,0 +1,56 @@
+//
+//  debtCell.swift
+//  SharingSpendings
+//
+//  Created by User on 30.10.2018.
+//  Copyright © 2018 Jakub Zagórowicz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DebtCell: UITableViewCell{
+    
+    var fromLabel = UILabel()
+    var toLabel = UILabel()
+    var howMuchLabel = UILabel()
+    
+    func setDebt(debt: Debt){
+        fromLabel.text = debt.from.name
+        fromLabel.textColor = EsteticsModel.inCellTextColor
+        fromLabel.font = UIFont.systemFont(ofSize: TableViewModel.inCellFontSize)
+        
+        toLabel.text = debt.to.name
+        toLabel.textColor = EsteticsModel.inCellTextColor
+        toLabel.font = UIFont.systemFont(ofSize: TableViewModel.inCellFontSize)
+        
+        howMuchLabel.text = String(format: EsteticsModel.numberFormat, debt.howMuch)
+        howMuchLabel.textColor = EsteticsModel.inCellTextColor
+        howMuchLabel.font = UIFont.systemFont(ofSize: TableViewModel.inCellFontSize)
+        
+        setLabelsConstraints()
+    }
+    private func setLabelsConstraints(){
+        
+        addSubview(fromLabel)
+        fromLabel.translatesAutoresizingMaskIntoConstraints = false
+        fromLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        fromLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1).isActive = true
+        fromLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        fromLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3).isActive = true
+        
+        addSubview(howMuchLabel)
+        howMuchLabel.translatesAutoresizingMaskIntoConstraints = false
+        howMuchLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        howMuchLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        howMuchLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3).isActive = true
+        howMuchLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1).isActive = true
+        
+        addSubview(toLabel)
+        toLabel.translatesAutoresizingMaskIntoConstraints = false
+        toLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        toLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1).isActive = true
+        toLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3).isActive = true
+        toLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
+    }
+}
